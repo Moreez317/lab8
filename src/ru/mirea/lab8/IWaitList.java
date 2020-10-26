9b45f98439884a2596ff5baebba7c6709f1e8d03
alexander <alexander31777@gmail.com>
2020-10-26 12:36:31 +0300

    Initial commit

---
+++ src/ru/mirea/lab8/IWaitList.java
@@ -0,0 +1,16 @@
+package ru.mirea.lab8;
+
+import java.util.Collection;
+
+interface IWaitList<E> {
+
+    void add(E element);
+
+    E remove();
+
+    boolean contains(E element);
+
+    boolean containsAll(Collection<E> c);
+
+    boolean isEmpty();
+}
